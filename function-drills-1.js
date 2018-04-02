46a92c14cc85a3c329ba5b60c1ae6a63b7b64771
Marina Dargitz <marina.dargitz@gmail.com>
2018-04-02 12:04:26 -0600

    add error catching

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,15 +1,19 @@
 'use strict';
 
 function yearOfBirth (age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
   return 2018-age;
 }
 
 function whoAmI (name, age) {
- 
-  console.log(`Hi, my name is ${name} and I am ${age} years old.`);
-  console.log(`I was born in ${yearOfBirth(age)}.`);
+  try {
+    console.log(`Hi, my name is ${name} and I am ${age} years old.`);
+    console.log(`I was born in ${yearOfBirth(age)}.`);
+  }
+  catch(e) { console.log(e.message);}
 }
 
 
-
-whoAmI('Dom', 33);
+whoAmI('Dom', -33);
